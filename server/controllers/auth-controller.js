d55bf025558a4b83d3151e139c3e1c73cebb73e5
rohit1620 <malavrohit1620@gmail.com>
2025-07-21 18:00:25 +0530

    error solve

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user-model')
+
 const home=async(req,res)=>{
     try {
         res.status(200).send("This is route home page again")
@@ -20,8 +22,18 @@ const contact=async(req,res)=>{
 
 const register=async(req,res)=>{
     try {
-        console.log(req.body);
-        res.status(200).json({message:req.body})
+       const {username,email,phone,password}=req.body;
+
+    //    const userExist= await User.findOne({email:email});
+    //    console.log("email",userExist);
+       
+    //    if(userExist){
+    //     return res.status(400).json({msg:"email already exist"});
+    //    }
+    const raw= await User.create({username,email,phone,password});
+  console.log(raw)
+raw.save()
+      res.status(200).json({msg:"data user"})
     } catch (error) {
         console.log(error);
         
